f3488a2480ba0513ac7d9620a948b7eaf5b965f5
unknown <oswalawender@gmail.com>
2024-03-06 21:57:54 +0100

    Bug Fixy i Komentarze

--- game.py
+++ game.py
@@ -21,37 +21,38 @@ dir_y=0 #Y Direction of the snake
 block_size=20; #Size of a single block
 number_of_blocks=40; #How manty blocks in X and Y
 snake_length=2; #Snake length
-snake_List = [] 
+snake_List = [] #List of snake compartments
 
-screen_size=(number_of_blocks*block_size)
+screen_size=(number_of_blocks*block_size) #Size of the game window
+screen = pygame.display.set_mode([screen_size,screen_size]) #Initialization of the window
+score_font = pygame.font.SysFont("comicsansms", 20) #Font for the score
+clock = pygame.time.Clock() #Clock initialization
+pygame.display.set_caption('PBSnakeAI') #Window name
+player_pos = pygame.Vector2(round(number_of_blocks/4)*20) #Starting player position
+food = pygame.Vector2(random.randint(1,number_of_blocks-1)*20,random.randint(1,number_of_blocks-1)*20) #Starting food position
 
-screen = pygame.display.set_mode([screen_size,screen_size])
-score_font = pygame.font.SysFont("comicsansms", 20)
-clock = pygame.time.Clock()
-pygame.display.set_caption('PBSnakeAI')
-player_pos = pygame.Vector2(random.randint(1,round(number_of_blocks-1/2))*20, random.randint(1,number_of_blocks-1)*20)
-food = pygame.Vector2(random.randint(1,number_of_blocks-1)*20,random.randint(1,number_of_blocks-1)*20)
-
-
+#Main game loop
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
-
+            running = False   
+                    
+    #Movement
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
+    if keys[pygame.K_w] and dir_y!=1:
         dir_x=0
         dir_y=-1
-    if keys[pygame.K_s]:
+    if keys[pygame.K_s] and dir_y!=-1:
         dir_x=0
         dir_y=1
-    if keys[pygame.K_a]:
+    if keys[pygame.K_a] and dir_x!=1:
         dir_x=-1
         dir_y=0
-    if keys[pygame.K_d]:
+    if keys[pygame.K_d] and dir_x!=-1:
         dir_x=1
         dir_y=0
 
+    #Moving snake head
     player_pos.x+=block_size*dir_x
     player_pos.y+=block_size*dir_y
 
@@ -60,26 +61,30 @@ while running:
     snake_Head.append(player_pos.y)
     snake_List.append(snake_Head)
 
+    #Screen edges handler
     if(player_pos.x>=screen_size-18 or player_pos.x<=-2 or player_pos.y>=screen_size-18 or player_pos.y<=-2):
         print("koniec")
         running=False
  
+    #Removing tail
     if len(snake_List) > snake_length:
             del snake_List[0]
 
+    #Snake drawing
     our_snake(block_size, snake_List)
-    pygame.draw.rect(screen,"green",[food.x+5,food.y+5,10,10])
-    
+
+    #Scoreboard handling
     Your_score(snake_length - 2,dt)
 
+    #Food spawn and draw
     if player_pos.x == food.x and player_pos.y == food.y:
         snake_length+=1
-        food = pygame.Vector2(random.randint(0,number_of_blocks)*20,random.randint(0,number_of_blocks)*20)
+        food = pygame.Vector2(random.randint(1,number_of_blocks-1)*20,random.randint(1,number_of_blocks-1)*20)
+    pygame.draw.rect(screen,"green",[food.x+5,food.y+5,10,10])
 
     pygame.display.flip()
-
     screen.fill("black") #Clearing screen
-    dt+=1
-    clock.tick(block_size)
+    dt+=1 #Tick coutner
+    clock.tick(block_size)#Setting tickrate
 
 pygame.quit()
